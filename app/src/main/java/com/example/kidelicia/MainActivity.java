9bc067c3f27be16f3a5664a12bbbd72908e8abb8
kaioMx <kaiomaximoaraujo@gmail.com>
2024-07-22 16:14:03 -0300

    22/07 termino de todas telas aa

--- app/src/main/java/com/example/kidelicia/MainActivity.java
+++ app/src/main/java/com/example/kidelicia/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kidelicia;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+    setContentView(R.layout.jornal
+        );
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Tjornal), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
